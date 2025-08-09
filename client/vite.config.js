a103bf810f15197b73bd82e46cae5a3a0752e810
“vinit <vinitpatel7840@gmail.com>
2025-08-09 12:56:31 +0530

    Fix PostCSS configuration for Vercel deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   css: {
-    postcss: './postcss.config.cjs'
+    postcss: {
+      config: {
+        path: './postcss.config.cjs'
+      }
+    }
   },
   build: {
     outDir: 'dist',
